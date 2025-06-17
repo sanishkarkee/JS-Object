a79a88cc9009f74749532610a4f1bc1acd4045d5
sanish <s.karki1994@gmail.com>
2025-06-17 15:45:37 +0545

    Created object using factory function

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 console.log('<< ---------- LEARNING OBJECT ( zero to Advanced ) ---------- >>');
 
-let user = {
-  name: 'David',
-  age: 25,
-  'is admin': true,
-};
+// let user = {
+//   name: 'David',
+//   age: 25,
+//   'is admin': true,
+// };
 
 // console.log(user);
 // console.log('User name is: ', user.name); // David
 // console.log('Age is: ', user.age); // 40
 
 // ---------- Adding new properties to OBJECT
-user.isSeniorCitizen = false;
+// user.isSeniorCitizen = false;
 // console.log('Are you old man? ', user.isSeniorCitizen); // false
 // console.log('Are you admin? ', user['is admin']);
 
@@ -20,29 +20,43 @@ user.isSeniorCitizen = false;
 // user['is admin'] = false;
 
 // delete user['is admin'];
-console.log(user);
+// console.log(user);
 
 // ---------- Accessing dynamic key value
-const someKey = 'age';
+// const someKey = 'age';
 
-console.log(user[someKey]); // 25
+// console.log(user[someKey]); // 25
 
 // ---------- CREATING OBJECT:  Create object with dynamic values
 // let car = 'carName';
-
 // let favCars = {
 //   [car]: 5,
 // };
 // console.log(favCars);
 
-// ---------- CREATING OBJECT:  Constructor function- convention- name should start with CAPITAL Letter
-function Car(name, model) {
-  this.name = name;
-  this.model = model;
-}
-const hondaCar = new Car('Sanish', 'Honda');
-const toyotaCar = new Car('Puskar', 'Toyota');
-console.log(hondaCar);
-console.log(toyotaCar);
+// ---------- CREATING OBJECT:  Constructor function, convention: name should start with CAPITAL Letter
+// function Car(name, model) {
+//   this.name = name;
+//   this.model = model;
+// }
+// const hondaCar = new Car('Sanish', 'Honda');
+// const toyotaCar = new Car('Puskar', 'Toyota');
+// console.log(hondaCar);
+// console.log(toyotaCar);
 
 // ----------CREATING OBJECT: using OBJECT() constructor
+// const person = new Object();
+// person.name = 'Micheal';
+// person.age = 35;
+// console.log(person);
+
+// ----------CREATING OBJECT: using FACTORY FUNCTION
+function createUser(name, age) {
+  return {
+    name,
+    age,
+  };
+}
+const user1 = createUser('Sanish', 35);
+const user2 = createUser('Puskar', 56);
+console.log(user1, user2);
